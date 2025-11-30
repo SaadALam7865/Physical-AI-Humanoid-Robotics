0082a8454f0f4a50b5de857118a3be4843fcdf73
uneezaismail <160355516+uneezaismail@users.noreply.github.com>
2025-11-30 19:18:44 +0500

    baseUrl in docusaurus.config.ts
    Changed base URL for deployment configuration.

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://uneezaismail.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/physical-ai-and-humanoid-robotics/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   organizationName: "uneezaismail", // GitHub username
